a67ff1245b58fa9ac3c0b34f899781214aabdb16
isaac43 <isaac.haik@gmail.com>
2024-03-18 01:32:44 +0100

    model

--- isaac/submission_isaac.py
+++ isaac/submission_isaac.py
@@ -5,7 +5,8 @@
 import time
 from pathlib import Path
 import pickle
-
+import pandas as pd
+import numpy as np
 import utils_isaac as utils
 
 
@@ -16,6 +17,45 @@ TRAINED_MODEL_DIR = Path('/trained_model/')
 TEST_DATA_DIR = Path('/dataset/test/')
 TEST_PREDS_FP = Path('/submission/submission.csv')
 
+
+def do_prediction(model, data, thresh, thresh_add):
+    def top_k(x, k):
+        ind = np.argpartition(x, -1*k)[-1*k:]
+        return ind[np.argsort(x[ind])]
+
+    def top_k_values(x, k):
+        ind = np.argpartition(x, -1*k)[-1*k:]
+        return x[ind][np.argsort(x[ind])]
+
+    pred_proba = pd.DataFrame(model.predict(
+        data, prediction_type='Probability'))
+    pred = pred_proba.idxmax(1)
+    print('Num of ex to cut', sum(pred_proba.max(1) < thresh))
+    nothing_index = pred.value_counts().index[0]
+    pred.loc[pred_proba.max(1) < thresh] = nothing_index
+
+    top_proba = pd.DataFrame(np.apply_along_axis(
+        lambda x: top_k(x, 2), 1, pred_proba.to_numpy()))
+    top_proba_values = pd.DataFrame(np.apply_along_axis(
+        lambda x: top_k_values(x, 2), 1, pred_proba.to_numpy()))
+    # print(top_proba)
+    # print(pred_proba*100)
+    # print(top_proba_values*100)
+    compt = 0
+    for i in range(len(top_proba)):
+        if top_proba.iloc[i, 1] == nothing_index:
+            if top_proba_values.iloc[i, 0] > thresh_add:
+                compt += 1
+                pred.iloc[i] = top_proba.iloc[i, 0]
+    print(compt)
+    pred = pred.to_numpy().reshape(-1, 1)
+    return pred
+
+
+threshold_ew = 0.1
+threshold_ns = 0.1
+tresh_add_ew = 0.11
+tresh_add_ns = 0.26
 
 # Rest of configuration, specific to this submission
 feature_cols = [
@@ -44,17 +84,32 @@ test_data, updated_feature_cols = utils.tabularize_data(
 # Load the trained models (don't use the utils module, use pickle)
 model_EW = pickle.load(open(TRAINED_MODEL_DIR / 'model_EW.pkl', 'rb'))
 model_NS = pickle.load(open(TRAINED_MODEL_DIR / 'model_NS.pkl', 'rb'))
+model_NS_preprocess = pickle.load(
+    open(TRAINED_MODEL_DIR / 'model_NS_preprocess.pkl', 'rb'))
 le_EW = pickle.load(open(TRAINED_MODEL_DIR / 'le_EW.pkl', 'rb'))
 le_NS = pickle.load(open(TRAINED_MODEL_DIR / 'le_NS.pkl', 'rb'))
 
+added_proba_feature_NS = pd.DataFrame(model_NS_preprocess.predict_proba(
+    test_data[updated_feature_cols])).add_prefix('proba_feature_NS_')
+added_proba_feature_NS.index = test_data.index
+test_data = pd.concat([test_data, added_proba_feature_NS], axis=1)
+
 # Make predictions on the test data for EW
 test_data['Predicted_EW'] = le_EW.inverse_transform(
-    model_EW.predict(test_data[model_EW.feature_names_])
+    do_prediction(
+        model_EW, test_data[model_EW.feature_names_], threshold_ew, tresh_add_ew)
 )
+
+added_proba_feature_EW = pd.DataFrame(model_EW.predict_proba(
+    test_data[updated_feature_cols+list(added_proba_feature_NS.columns)])).add_prefix('proba_feature_EW_')
+added_proba_feature_EW.index = test_data.index
+test_data = pd.concat([test_data, added_proba_feature_EW], axis=1)
+
 
 # Make predictions on the test data for NS
 test_data['Predicted_NS'] = le_NS.inverse_transform(
-    model_NS.predict(test_data[model_NS.feature_names_])
+    do_prediction(
+        model_NS, test_data[model_NS.feature_names_], threshold_ns, tresh_add_ns)
 )
 
 test_data['Predicted_EW'] = test_data['Predicted_EW'].mask(
